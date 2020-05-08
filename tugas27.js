994a8221595307e8fbc0a63d453ce2f2c70c9b73
Muhammad Aji Putra <muhammad.aji.putra.mp98@gmail.com>
2020-05-08 23:15:32 +0700

    tugas27js
    mengumpulkan tugas 27 js

---
+++ tugas27.js
@@ -0,0 +1,29 @@
+function tugas27() {
+    var x = "No";
+    var y = "rumah saya";
+    var z = 30;
+    var a = "A";
+
+    var b = "Jalan pangeran Tirtayasa No";
+    var c = 70;
+
+    d = String(z);
+    e = String(c);
+
+    f = x.concat(" " + y + " " + z + a);
+    console.log(f);
+    
+    console.log("");
+
+    g = b.concat(" " + c);
+    console.log(g);
+    
+    console.log("");
+    
+    h = f.concat(" " + g);
+    
+    console.log(h);
+    
+}
+
+tugas27()
